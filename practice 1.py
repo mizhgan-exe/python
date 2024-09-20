ad0f6a734abde93e9fe0381e5f9f1e6137eb1606
mizhgan-exe <140151178+mizhgan-exe@users.noreply.github.com>
2024-09-20 12:37:13 +0300

    practice 1.py

---
+++ practice 1.py
@@ -0,0 +1,71 @@
+#Задание 1
+first_num=9
+second_num=7.8
+my_str="start"
+print (first_num, second_num, my_str)
+
+first_num=5.2
+print (type(first_num), first_num)
+
+third_num=first_num + second_num
+print (type(third_num), third_num)
+
+first_num +=5
+second_num +=first_num
+print(first_num, second_num)
+
+print(int(second_num), first_num)
+
+my_str="start&stop"
+print(my_str)
+
+print(my_str * 5)
+
+#Задание 2
+path="C:\\Users\\MainAdmin\\Desktopn\\programs"
+print (path)
+
+path="C:\\Users\\MainAdmin\\Desktopn\\programs\\game.exe"
+print (path)
+
+path="C:\\Users\\MainAdmin\\Desktopn\\programs\\picture.png"
+print (path)
+
+path="C:\\Games\\city simulator"
+print (path*2)
+print("Error path:", path*2)
+
+#Задание 3
+num_1=7
+num_2=10
+num_3=4
+
+summ = num_1 + num_2 + num_3
+print("Сумма всех целых чисел", summ)
+
+num_1=7.9
+num_2=21.3 
+num_3=4.8 
+summ = num_1 + num_2 + num_3
+print("Умножение всех чисел", summ)
+
+num_1=130
+num_2=4
+mum_3=2
+myltiplaying = num_1 * num_2 * num_3
+print("Умножение всех чисел", myltiplaying)
+
+division = num_1 / num_2 / num_3
+print ("Деление:", division)
+
+num_1=2
+num_2=3 
+num_3=4 
+degree = num_1 ** num_2 ** num_3
+ptint ("Числа в степени:", degree)
+
+num_1=2
+num_2=8 
+num_3=5 
+math = (43 + num_1) + (num_2 + 67) - (num_3 * 2)
+print (math//2)
