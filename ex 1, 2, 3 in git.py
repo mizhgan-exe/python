5f405b8b9be6aab02cad631a8191b4f8d3f953a0
mizhgan-exe <140151178+mizhgan-exe@users.noreply.github.com>
2024-09-20 11:33:47 +0300

    Create ex 1, 2, 3 in git.py

---
+++ ex 1, 2, 3 in git.py
@@ -0,0 +1,26 @@
+#Задание 1
+integer_var=52
+float_var=1.23
+string_var="sheeesh"
+list_var=[1, 2, 3, 4, 5, 6, 7, 8, 9]
+tuple_var=(10, 20, 30)
+dict_var={"Pedro", 77}
+set_var={3, 2, 1} 
+bool_var=True
+print (integer_var, float_var, string_var, list_var, tuple_var, dict_var, set_var, bool_var)
+#Задание 2
+name="Mihalich"
+age="18"
+average_grade=4
+favorite_movies=["I DON'T LIKE FILMS, SORRY"]
+personal_info=["Chizhevskiy", 20, 4]
+skills={"jump": 99}
+visited_countries={"Turkey", "Dubai", "Thailand"}
+has_driver_license=False 
+print (name, age, average_grade, favorite_movies, personal_info, skills, visited_countries, has_driver_license)
+#Задание 3
+processor="intel"
+ram=16
+internet_speed=300
+pc=[processor, ram, internet_speed]
+print (pc)
